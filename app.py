3069af5f52a1acec4db376798e3b073c146ce6f3
Brendan Molloy <brendan@bbqsrc.net>
2013-05-27 23:01:19 +1000

    order

--- app.py
+++ app.py
@@ -90,10 +90,10 @@ if __name__ == "__main__":
 
     application = Application([
         (r"/static/(.*)", StaticFileHandler, {"path": "./static"}),
-        (r"/([^\.]+?)(?:\.(json))?", FactoidHandler),
-        (r"/", RandomFactoidHandler),
         (r"/random(?:\.(json))?", RandomFactoidHandler),
         (r"/all(?:\.(json))?", AllFactoidHandler),
+        (r"/", RandomFactoidHandler),
+        (r"/([^\.]+?)(?:\.(json))?", FactoidHandler),
     ])
     
     '''
